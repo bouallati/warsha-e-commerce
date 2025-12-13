bbde92dc4b68c6b0085aba00a69a560e29ede5fd
bouallati <bouallati.info@gmail.com>
2025-12-13 18:59:21 +0100

    feat: register category routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import productRoutes from "./product.routes.js";
+import categoryRoutes from "./category.routes.js";
 
 const routes = express.Router();
 
 routes.use("/api/products", productRoutes);
+routes.use("/api/categories", categoryRoutes);
 export default routes;
