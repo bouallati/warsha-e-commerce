19f16d0eb7c88b91eb81ed0a5d0ff175590cc33c
bouallati <bouallati.info@gmail.com>
2025-12-13 20:52:57 +0100

    feat: add cart controller

---
+++ src/controllers/cart.controller.js
@@ -0,0 +1,35 @@
+import cart from "../data/cart.js";
+import products from "../data/products.js";
+
+export const getCart = (req, res) => {
+  res.json(cart);
+};
+
+export const addItemToCart = (req, res) => {
+  const newItem = req.body;
+  if (!newItem.productId || !newItem.quantity) {
+    return res
+      .status(400)
+      .json({ message: "productId and quantity are required" });
+  }
+
+  const product = products.find((p) => p.id === newItem.productId);
+
+  if (!product) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+
+  const existingItem = cart.items.find(
+    (item) => item.productId === newItem.productId
+  );
+
+  if (existingItem) {
+    existingItem.quantity += newItem.quantity;
+  } else {
+    cart.items.push(newItem);
+  }
+
+  cart.totalPrice += product.price * newItem.quantity;
+
+  res.status(201).json(cart);
+};
