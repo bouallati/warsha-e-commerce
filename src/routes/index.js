dc6c04647060a4134c202e3218ab003f73ca4e49
bouallati <bouallati.info@gmail.com>
2025-12-13 20:58:05 +0100

    feat: register cart routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import productRoutes from "./product.routes.js";
 import categoryRoutes from "./category.routes.js";
+import cartRoutes from "./cart.route.js";
 
 const routes = express.Router();
 
 routes.use("/api/products", productRoutes);
 routes.use("/api/categories", categoryRoutes);
+routes.use("/api/cart", cartRoutes);
 export default routes;
